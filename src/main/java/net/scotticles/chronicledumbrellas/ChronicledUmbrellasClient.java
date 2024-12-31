1d1e2bd351744e86eaa53476037b91ae0360f5c5
Scotticles <itsellaneous@gmail.com>
2024-12-31 01:05:02 -0500

    Initial Upload Commit

---
+++ src/main/java/net/scotticles/chronicledumbrellas/ChronicledUmbrellasClient.java
@@ -0,0 +1,10 @@
+package net.scotticles.chronicledumbrellas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChronicledUmbrellasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
